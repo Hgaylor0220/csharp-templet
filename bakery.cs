a677f01a5e12e1e63d39dc7c8bcd8a6acf510b64
Hailey Gaylor <haileygaylor@gmail.com>
2019-09-27 16:18:15 -0700

    added front end logic to take info from the user

---
+++ bakery.cs
@@ -0,0 +1,16 @@
+using System;
+namespace products
+{
+    class pastrys 
+    {
+    public int Price { get; set;}
+    public int Quanity { get; set;}
+    
+    }
+    class bread
+    {
+        public int Price { get; set; }
+        public int Quanity { get; set; }
+
+    }
+}
--- project.cs
+++ project.cs
@@ -1,8 +1,40 @@
 using System;
+using Bakery;
 
-class Program
+public class Program
 {
-  static void Main()
+ public static void Main()
   {
+    Bread customerBreadOrder = new Bread(price,quanity);
+    Pastry customerPastryOrder = new Pastry(Predicate,quanity);
+
+    Console.WriteLine("Hello! Welcome to Pierre's Bakery! Wye have Pastries and Bread specials on the daily! would you like to take a look around? [Y for yes or N for no] ");
+    string Lookaround = Console.ReadLine();
+      if(Lookaround.ToLower == "y")
+      {
+        Console.WriteLine("Wonderful!!! Let me tell you about my specials while I have you! We have a great deal going on for our Pastries one for $2 or 3 for $5!! our bread is going for buy two get on at a whomping price of just $5 per loaf!");
+        Console.WriteLine("Would you like to buy any bread or Pastries? maybe both?");
+        string UserOrder = Console.ReadLine();
+          if(UserOrder.ToLower == "bread")
+          {
+            customerBreadOrder.calculateCost;
+            
+          }
+          if (UserOrder.ToLower == "pastry")
+          {
+            Console.WriteLine("Great Choice! We have lots of selections heres a few: Croissants, Danishes, Cannoli, Strudels and Eclairs");
+            customerPastryOrder.calculateCost;
+            
+          }
+          if (UserOrder.ToLower == "both")
+          {
+              customerBreadOrder.calculateCost;
+              customerPastryOrder.calculateCost;
+          }
+      }
+      else 
+      [
+        Console.WriteLine("Thank you for stopping buy! Please come visit us again");
+      ]
   }
 }
