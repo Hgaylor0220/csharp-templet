065539df760c37a383c09ac0cc54883ef6f93e66
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-03 19:21:37 -0700

    added readMe and gitIgnore file

--- bakery.cs
+++ bakery.cs
@@ -1,16 +1,64 @@
 using System;
-namespace products
+namespace Products
 {
-    class pastrys 
+    class Pastrys 
     {
-    public int Price { get; set;}
-    public int Quanity { get; set;}
+        private int Price = 2;
+        private int PriceBreakAmount = 3;
+        private int PriceBreakPrice = 5;
+
+        public int quantity;
+
+        public Pastrys (int orderQuantity) {
+            
+            quantity = orderQuantity;
+            Console.WriteLine("You want " + orderQuantity + " pastries.");
+        }
+
+        public int ReturnCost (){
+            int priceBreakTotal = Decimal.ToInt32(
+                quantity / PriceBreakAmount);
+            Console.WriteLine("priceBreakTotal: " + priceBreakTotal);
+            int regularTotal = 
+            quantity - (priceBreakTotal * PriceBreakAmount);
+            Console.WriteLine("regularTotal: " + regularTotal);
+
+            int cost = (regularTotal * 
+            Price) + (priceBreakTotal * 
+            PriceBreakPrice);
+            Console.WriteLine("cost: " + cost);
+            return cost;
+        }
     
     }
-    class bread
+    class Bread
     {
-        public int Price { get; set; }
-        public int Quanity { get; set; }
+        private int Price = 5;
+        private int PriceBreakAmount = 3;
+        private int PriceBreakPrice = 10;
+
+        public int quantity;
+
+        public Bread(int orderQuantity)
+        {
+            
+            quantity = orderQuantity;
+            Console.WriteLine("You want " + orderQuantity + " loaves of bread.");
+        }
+
+        public int ReturnCost()
+        {
+            
+            int priceBreakTotal = Decimal.ToInt32(
+                quantity / PriceBreakAmount);
+            int regularTotal = 
+            quantity - (priceBreakTotal * PriceBreakAmount);
+
+            int cost = (regularTotal * 
+            Price) + (priceBreakTotal * 
+            PriceBreakPrice);
+            return cost;
+        }
 
     }
 }
--- project.cs
+++ project.cs
@@ -1,40 +1,31 @@
 using System;
-using Bakery;
+using Products;
 
 public class Program
 {
  public static void Main()
   {
-    Bread customerBreadOrder = new Bread(price,quanity);
-    Pastry customerPastryOrder = new Pastry(Predicate,quanity);
-
-    Console.WriteLine("Hello! Welcome to Pierre's Bakery! Wye have Pastries and Bread specials on the daily! would you like to take a look around? [Y for yes or N for no] ");
+   
+    Console.WriteLine("Hello! Welcome to Pierre's Bakery! We have Pastries and Bread specials on the daily! We have bread for buy two get one free at $5 per loaf and pastries 3 for $5 or $2 each, would you like to make a purchase? [Y for yes or N for no] ");
     string Lookaround = Console.ReadLine();
-      if(Lookaround.ToLower == "y")
-      {
-        Console.WriteLine("Wonderful!!! Let me tell you about my specials while I have you! We have a great deal going on for our Pastries one for $2 or 3 for $5!! our bread is going for buy two get on at a whomping price of just $5 per loaf!");
-        Console.WriteLine("Would you like to buy any bread or Pastries? maybe both?");
-        string UserOrder = Console.ReadLine();
-          if(UserOrder.ToLower == "bread")
-          {
-            customerBreadOrder.calculateCost;
-            
-          }
-          if (UserOrder.ToLower == "pastry")
-          {
-            Console.WriteLine("Great Choice! We have lots of selections heres a few: Croissants, Danishes, Cannoli, Strudels and Eclairs");
-            customerPastryOrder.calculateCost;
-            
-          }
-          if (UserOrder.ToLower == "both")
-          {
-              customerBreadOrder.calculateCost;
-              customerPastryOrder.calculateCost;
-          }
-      }
-      else 
-      [
-        Console.WriteLine("Thank you for stopping buy! Please come visit us again");
-      ]
+
+    if(Lookaround.ToLower() == "y")
+    {
+      Console.WriteLine("Wonderful!!! How many loafs of bread would you like,They are Buy 2, get 1 free. A single loaf costs $5?");
+      string UserBreadOrder = Console.ReadLine();
+      Console.WriteLine("And how many pastries would you like? They are 3 for $5 or $2 each.");
+      string UserPastryOrder = Console.ReadLine();
+
+      Pastrys pastry_order = new Products.Pastrys(Convert.ToInt32(UserPastryOrder));
+      Bread bread_order = new Products.Bread(Convert.ToInt32(UserBreadOrder));
+
+      int total = pastry_order.ReturnCost() + bread_order.ReturnCost();
+
+      Console.WriteLine("Your total comes to: $" + total);
+    }
+    else
+    {
+      Console.WriteLine("Thank you for stopping buy! Please come visit us again");
+    }
   }
 }
